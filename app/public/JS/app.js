9c7665136d3d83979319f4e368bbcf63547ac475
Sam Llamas <sam.llamas@utexas.edu>
2018-06-29 12:56:24 -0500

    changes to skills, and making fancy

--- app/public/JS/app.js
+++ app/public/JS/app.js
@@ -1,13 +1,30 @@
+
+
 $('.nav-link, .nav-logo').on('click', function (event) {
     event.preventDefault()
     var elem = $(this.hash)
-    var offsetAmount = parseInt(elem.find(">:first-child").css('padding-top'),10)
-    if (offsetAmount === 0){
-        offsetAmount = parseInt(elem.css("padding-top"),10)
+    var offsetAmount = parseInt(elem.find(">:first-child").css('padding-top'), 10)
+    if (offsetAmount === 0) {
+        offsetAmount = parseInt(elem.css("padding-top"), 10)
     }
     console.log(elem, offsetAmount)
     $('html, body').animate(
-        { scrollTop: (elem.offset().top - offsetAmount)},
+        { scrollTop: (elem.offset().top - offsetAmount) },
         { duration: 1000, easing: 'swing' }
     );
 });
+
+
+const test = document.querySelector("#test");
+const text = test.innerHTML.split("");
+test.innerHTML = "";
+let index = 0;
+setTimeout(function(){
+    const clearId = setInterval(() => {
+        test.innerHTML = test.innerHTML + text[index];
+        index++;
+        if (index >= text.length) {
+            clearInterval(clearId);
+        }
+    }, 150)
+}, 500)
